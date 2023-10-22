a04d2555be46bffa916617e609cd86a020ccc310
Michael Holm <michael@weblovers.se>
2023-10-22 23:53:54 +0200

    Trying fix

--- src/components/Album/Artists.jsx
+++ src/components/Album/Artists.jsx
@@ -1,26 +1,7 @@
 import { Artist } from "./artist"
 
 export const Artists = ({artists}) => {
-  const artistSeparator = (artistCount, index) => {
-    if (index === artistCount - 2) {
-      // If we have 1 artist this will never match as index would have to be -1
-      // If we have 2 artists this will match for the first artist
-      // If we have 3 artists this will match for the second artist
-      return " & ";
-    } else if (index < artistCount - 2) {
-      // if we didn't get a match above we continue on.
-      // If we have 1 artist this will never match as index would have to be -2
-      // If we have 2 artists this will never match as index would have to be -1
-      // If we have 3 artists this will match for the first artist
-      // If we have 4 artists this will match for the first and second artist
-      return ", ";
-    } else {
-      // if none of the rules above matches we return a empty string
-      // If we have 1 artist this will match this artist
-      // If we have 2 artists this will match the second artist
-      // If we have 3 artists this will match the third artist
-      return "";
-    }
+  return ""
   }
 
   return (
